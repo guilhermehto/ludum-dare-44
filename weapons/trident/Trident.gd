0eb51e12df14d452bfd0e468887e4030e68671e4
Guilherme Oliveira <htoguilherme@gmail.com>
2019-04-28 18:48:03 -0300

    Trident now looks at the mouse

--- weapons/trident/Trident.gd
+++ weapons/trident/Trident.gd
@@ -5,15 +5,18 @@ export var knock_back_force := 15.0
 export var hit_delay := 0.15
 
 
-func _unhandled_key_input(event: InputEventKey) -> void:
-	if event.is_action_pressed("move_up") and rotation_degrees != 0:
-		rotation_degrees = 0
-	elif event.is_action_pressed("move_down") and rotation_degrees != -180:
-		rotation_degrees = -180
-	elif event.is_action_pressed("move_left") and rotation_degrees != -90:
-		rotation_degrees = -90
-	elif event.is_action_pressed("move_right") and rotation_degrees != -270:
-		rotation_degrees = -270
+func _process(delta: float) -> void:
+	look_at(get_global_mouse_position())
+
+#func _unhandled_key_input(event: InputEventKey) -> void:
+#	if event.is_action_pressed("move_up") and rotation_degrees != 0:
+#		rotation_degrees = 0
+#	elif event.is_action_pressed("move_down") and rotation_degrees != -180:
+#		rotation_degrees = -180
+#	elif event.is_action_pressed("move_left") and rotation_degrees != -90:
+#		rotation_degrees = -90
+#	elif event.is_action_pressed("move_right") and rotation_degrees != -270:
+#		rotation_degrees = -270
 
 
 func _on_Area2D_body_entered(body: PhysicsBody2D) -> void:
