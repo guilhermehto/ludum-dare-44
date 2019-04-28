c8ff15ca4118239aefbc9a0b97ee29cc52080076
Guilherme Oliveira <htoguilherme@gmail.com>
2019-04-28 15:02:43 -0300

    Limited spawn count by spawners

--- levels/spawner/Spawner.gd
+++ levels/spawner/Spawner.gd
@@ -6,10 +6,13 @@ onready var tween : Tween = $Tween
 onready var timer : Timer = $Timer
 
 export var persons := []
+export var max_spawn_count := 3
 export(float, 5.0, 15.0) var max_spawn_interval := 7.5
+
 
 const Y_OFFSET = 100
 
+var currently_spawned := 0
 
 func _ready() -> void:
 	assert persons.size() > 0
@@ -21,11 +24,14 @@ func _ready() -> void:
 func _on_Timer_timeout() -> void:
 	timer.wait_time = rand_range(max_spawn_interval * 0.5, max_spawn_interval)
 	timer.start()
-	spawn()
+	if currently_spawned < max_spawn_count:
+		spawn()
 
 
 func spawn() -> void:
+	currently_spawned += 1
 	var new_person = persons[randi() % persons.size()].instance()
+	new_person.connect("punished", self, "_on_Person_punished")
 	add_child(new_person)
 	emit_signal("spawned", new_person)
 	new_person.global_position = global_position + Vector2.UP * Y_OFFSET
@@ -39,3 +45,7 @@ func spawn() -> void:
 	tween.start()
 	yield(tween, "tween_completed")
 	new_person.active = true
+
+
+func _on_Person_punished() -> void:
+	currently_spawned -= 1
