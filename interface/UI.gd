70bb401e795b348ad4d21c60b7e30495032fd5f8
Guilherme Oliveira <htoguilherme@gmail.com>
2019-04-28 18:30:51 -0300

    Combo added

--- interface/UI.gd
+++ interface/UI.gd
@@ -1,7 +1,10 @@
 extends CanvasLayer
 
 onready var punished : Label = $Control/Panel/Punished
+onready var combo : Label = $Control/Panel/Combo
 onready var time : Label = $Control/Panel/Time
+onready var tween : Tween = $Tween
+
 
 var level_time := 0.0
 var punished_goal := 0
@@ -31,3 +34,28 @@ func set_time_text(time_in_seconds: float) -> void:
 func set_punished_count(value: int) -> void:
 	punished_count = value
 	punished.text = "%s/%s" % [str(punished_count), str(punished_goal)]
+
+
+func _on_Level_combo_ended() -> void:
+	combo.hide()
+
+
+func _on_Level_comboed(count) -> void:
+	combo.show()
+	combo.text = "COMBO\n%s" % count
+	tween.interpolate_property(combo,
+		"rect_scale",
+		Vector2(0.5, 0.5),
+		Vector2(0.75, 0.75),
+		0.25,
+		Tween.TRANS_BOUNCE,
+		Tween.EASE_OUT)
+	tween.interpolate_property(combo,
+		"rect_scale",
+		Vector2(0.75, 0.75),
+		Vector2(0, 0),
+		3.0,
+		Tween.TRANS_QUINT,
+		Tween.EASE_OUT,
+		0.25)
+	tween.start()
--- levels/Level.gd
+++ levels/Level.gd
@@ -2,21 +2,27 @@ extends Node2D
 
 signal won
 signal failed
+signal comboed(count)
+signal combo_ended
 
 onready var ui : CanvasLayer = $UI
 onready var timer : Timer = $Timer
 onready var player : Player = $Player
 onready var spawners : Node2D = $Spawners
+onready var combo_timer : Timer = $ComboTimer
 
 export var level_time := 60.0
 export var persons_to_punish := 27
+export var combo_breaker_time := 1.25
 
 var punished_persons := 0.0
 var time_since_last_punishment := 0
+var combo := 0
 
 
 func _ready() -> void:
 	timer.wait_time = level_time
+	combo_timer.wait_time = combo_breaker_time
 	timer.start()
 	ui.initialize(level_time, persons_to_punish)
 	for spawner in spawners.get_children():
@@ -33,7 +39,12 @@ func _on_Timer_timeout() -> void:
 
 func _on_Person_punished() -> void:
 	time_since_last_punishment = 0
+	combo += 1
+	if combo > 1:
+		emit_signal("comboed", combo)
 	punished_persons += 1
+	combo_timer.stop()
+	combo_timer.start()
 	ui.punished_count += 1
 	if punished_persons == persons_to_punish:
 		emit_signal("won")
@@ -43,3 +54,9 @@ func _on_Person_punished() -> void:
 func _on_Spawner_spawned(person) -> void:
 		person.initialize(player)
 		person.connect("punished", self, "_on_Person_punished")
+
+
+func _on_ComboTimer_timeout() -> void:
+	combo = 0
+	combo_timer.stop()
+	emit_signal("combo_ended")
